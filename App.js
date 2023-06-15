ab2e8dcf0e7f074157d7dc8c63246a8485beaa75
MonsterLuni <monsterluni@gmail.com>
2023-06-15 14:00:37 +0200

    feat MusicPlayer: You can now Pause/Resume Sounds

--- App.js
+++ App.js
@@ -7,30 +7,37 @@ export default function App() {
 
   const [sound, setSound] = useState();
   var [Status, setStatus] = useState(false);
+  var [Statusmessage, setStatusmessage] = useState("Play Song");
+  var [loading, setloading] = useState(true);
 
   async function changestateofSound() {
     if(Status == true){
-      sound.stopAsync();
+      sound.pauseAsync();
       console.log("GESTOPPT");
+      setStatusmessage("Resume Song")
       setStatus(false);
     }
     else{
-      console.log('Loading Sound');
-      const {sound} = await Audio.Sound.createAsync(require("./assets/Music/Resonance.mp3"));
-      setSound(sound);
-      console.log('Playing Sound');
-      await sound.playAsync();
-      console.log(Status);
-    
+      if(loading == true){
+        console.log('Loading Sound');
+        const {sound} = await Audio.Sound.createAsync(require("./assets/Music/Drinking_Water.mp3"));
+        setSound(sound);
+        console.log('Playing Sound');
+        await sound.playAsync();
+        setloading(false);
+      }
+      else{
+        sound.playAsync();
+      }
+      setStatusmessage("Pause Song")
       setStatus(true);
-      console.log(Status);
     }
   }
 
   return (
     <View style={styles.container}>
       <Text>Dies wird der MusicPlayer!</Text>
-      <Button title="Play Sound" onPress={changestateofSound} />
+      <Button title={(Statusmessage)} onPress={changestateofSound} />
       <StatusBar style="auto" />
     </View>
   );
