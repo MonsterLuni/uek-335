6499ad7388c5ba8cd12664771c3f5747ea2ff815
Yamato Semling <yamato.semling@bict-edu.ch>
2023-06-15 11:32:17 +0200

    feat: create camera funtion, camera capture picture

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet ,Text, View, Button, Image, TouchableOpacity} from 'react-native';
+import { Camera } from 'expo-camera';
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.front);
+useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+})();
+  }, []);
+const takePicture = async () => {
+    if(camera){
+        const data = await camera.takePictureAsync(null)
+        setImage(data.uri);
+    }
+  }
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <View style={{ flex: 1}}>
+      <View style={styles.cameraContainer}>
+            <Camera 
+            ref={ref => setCamera(ref)}
+            style={styles.fixedRatio} 
+            type={type}
+            ratio={'16:9'} />
+      </View>
+      <TouchableOpacity style={styles.flip} onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+      <Image
+        style={styles.button}
+        source = {require('./icons/flip.png')}
+      />
+      </TouchableOpacity>
+        <View style={styles.bar}>
+          <View style={styles.btn}>
+          <TouchableOpacity style={styles.captureButton} onPress={takePicture}>
+            <FontAwesome name="camera" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      {image && <Image source={{uri: image}} style={{flex:1}}/>}
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  cameraContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      height: 100,
+  },
+  fixedRatio:{
+      flex: 1,
+  },
+  bar:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    position: 'absolute',
+    bottom: 40,
+    width: '100%',
+    height: 60,
+    backgroundColor: 'rgba(0, 0, 0, .7)',
+  },
+  btn:{
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
     justifyContent: 'center',
   },
-});
+  icons:{
+
+  },
+  flip: {
+    position: 'absolute',
+    top: 50,
+    right: 10, 
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    borderRadius: 50,
+    padding: 10,
+  },
+  captureButton: {
+    position: "absolute",
+    alignSelf: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    borderRadius: 50,
+    padding: 15,
+  },
+})
