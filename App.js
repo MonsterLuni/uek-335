5a2a6eedb504790f400b37531125889ee3ddfaf7
Yamato Semling <yamato.semling@bict-edu.ch>
2023-06-15 13:07:01 +0200

    feat: styles -> bar, photo button, flip button, preview image

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet ,Text, View, Button, Image, TouchableOpacity} from 'react-native';
-import { Camera } from 'expo-camera';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Camera } from "expo-camera";
 import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
@@ -8,87 +15,94 @@ export default function App() {
   const [camera, setCamera] = useState(null);
   const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.front);
-useEffect(() => {
+  useEffect(() => {
     (async () => {
       const cameraStatus = await Camera.requestCameraPermissionsAsync();
-      setHasCameraPermission(cameraStatus.status === 'granted');
-})();
+      setHasCameraPermission(cameraStatus.status === "granted");
+    })();
   }, []);
-const takePicture = async () => {
-    if(camera){
-        const data = await camera.takePictureAsync(null)
-        setImage(data.uri);
+  const takePicture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      setImage(data.uri);
     }
-  }
+  };
 
   if (hasCameraPermission === false) {
     return <Text>No access to camera</Text>;
   }
+
+  {
+    image && <Image source={{ uri: image }} style={{ flex: 1 }} />;
+  }
+
   return (
-   <View style={{ flex: 1}}>
+    <View style={{ flex: 1 }}>
       <View style={styles.cameraContainer}>
-            <Camera 
-            ref={ref => setCamera(ref)}
-            style={styles.fixedRatio} 
-            type={type}
-            ratio={'16:9'} />
+        <Camera
+          ref={(ref) => setCamera(ref)}
+          style={styles.fixedRatio}
+          type={type}
+          ratio={"16:9"}
+        />
       </View>
-      <TouchableOpacity style={styles.flip} onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-      <Image
-        style={styles.button}
-        source = {require('./icons/flip.png')}
-      />
+      <TouchableOpacity
+        style={styles.flip}
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}
+      >
+        <Image style={styles.button} source={require("./icons/flip.png")} />
       </TouchableOpacity>
-        <View style={styles.bar}>
-          <View style={styles.btn}>
+      <View style={styles.bar}>
+        <View style={styles.btn}>
           <TouchableOpacity style={styles.captureButton} onPress={takePicture}>
-            <FontAwesome name="camera" size={24} color="white" />
+            <FontAwesome name="camera" size={36} color="white" />
           </TouchableOpacity>
         </View>
       </View>
-      {image && <Image source={{uri: image}} style={{flex:1}}/>}
+      <TouchableOpacity onPress={takePicture}>
+        {image && <Image source={{ uri: image }} style={styles.pre_img} />}
+      </TouchableOpacity>
     </View>
   );
 }
 const styles = StyleSheet.create({
   cameraContainer: {
-      flex: 1,
-      flexDirection: 'row',
-      height: 100,
-  },
-  fixedRatio:{
-      flex: 1,
-  },
-  bar:{
     flex: 1,
-    position: 'absolute',
+    flexDirection: "row",
+    height: 100,
+  },
+  fixedRatio: {
+    flex: 1,
+  },
+  bar: {
+    flex: 1,
+    position: "absolute",
     bottom: 40,
-    width: '100%',
-    height: 60,
-    backgroundColor: 'rgba(0, 0, 0, .7)',
+    width: "100%",
+    height: 80,
+    backgroundColor: "rgba(0, 0, 0, .7)",
   },
-  btn:{
+  btn: {
     flex: 1,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
   },
-  icons:{
-
-  },
+  icons: {},
   flip: {
-    position: 'absolute',
+    size: 36,
+    position: "absolute",
     top: 50,
-    right: 10, 
+    right: 10,
     backgroundColor: "rgba(255, 255, 255, 0.4)",
     borderRadius: 50,
-    padding: 10,
+    padding: 15,
   },
   captureButton: {
     position: "absolute",
@@ -97,4 +111,12 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     padding: 15,
   },
-})
+  pre_img: {
+    position: "absolute",
+    height: 70,
+    width: 50,
+    bottom: 45,
+    left: 10,
+    borderRadius: 10,
+  },
+});
