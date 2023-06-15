a62ea0d1650bea1da710320e6eff7a7840434e7b
Moritz Henzi <moritz.henzi@bict-edu.ch>
2023-06-15 13:15:32 +0200

    feat: finished with styling, border radius isn't possible

--- App.js
+++ App.js
@@ -5,38 +5,29 @@ import { Button, StyleSheet, Text, View } from "react-native";
 import * as Location from 'expo-location';
 import { getDistance } from "geolib";
 
-
-
 export default function App() {
-  //const [result, setResult] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-  const[location , setLocation]= useState(null);
-  const[errorMsg , setErrorMsg]= useState(null);
-  useEffect( () => {
-    (async() => {
-      let {status}= await Location.requestForegroundPermissionsAsync();
-      if (status !=='granted')
-      {
-        setErrorMsg('Permission to access locatiob was denied');
-        return; 
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
       }
-      let location= await Location.getCurrentPositionAsync({});
+      let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
-  
     })();
+  }, []);
 
-  },[]);
-  let text= 'Waiting...';
-  
-  if(errorMsg)
-  {
-    text(errorMsg)
+  let text = 'Waiting...';
+
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
   }
-  else if(location) {
-   text = JSON.stringify(location);
-
-  }
-
 
   const bict_coords = {
     latitude: 46.95586,
@@ -46,10 +37,6 @@ export default function App() {
   };
 
   const mapRef = useRef(null);
-
-  const goToBict = () => {
-    mapRef.current.animateToRegion(bict_coords, 1 * 1000);
-  };
 
   const calculateDistance = () => {
     if (location) {
@@ -61,13 +48,11 @@ export default function App() {
       const distance = getDistance(currentCoords, bict_coords);
       const distanceInKm = distance / 1000;
 
-      if(distanceInKm < 0.05) {
-        return 'Present'
+      if (distanceInKm < 0.05) {
+        return 'Present';
+      } else {
+        return `${distanceInKm} KM`;
       }
-
-      else{return `${distanceInKm} KM`;}
-
-      
     }
 
     return '';
@@ -77,37 +62,28 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-
-      <Text>BiCT Distance: {distanceToBict}</Text>
-
+      <View style={styles.header}>
+        <Text style={styles.title}>BiCT Distance</Text>
+        <View style={styles.separator} />
+      </View>
+      <Text style={styles.distance}>{distanceToBict}</Text>
       <MapView
-      ref={mapRef}
-      showsMyLocationButton= {true}
-      showsUserLocation= {true}
-      style={styles.map}
-      initialRegion={{
-        latitude: 46.95586,
-        longitude: 7.47572,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      }}
-      
+        ref={mapRef}
+        showsMyLocationButton={true}
+        showsUserLocation={true}
+        style={styles.map}
+        initialRegion={{
+          latitude: 46.95586,
+          longitude: 7.47572,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
       >
-
         <Marker coordinate={bict_coords} style={styles.marker}>
           <CustomMarker />
         </Marker>
-        
       </MapView>
-
-      <Button
-        onPress={() => goToBict()}
-        title="Nach BiCT"
-        style={styles.home}
-      ></Button>
-        
-
-
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -125,21 +101,35 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    paddingTop: 60,
+    paddingBottom: 20,
+  },
+  header: {
+    alignItems: "center",
   },
   title: {
-    fontSize: 40,
+    fontSize: 24,
+    fontWeight: "bold",
     color: "#30348c",
+    marginBottom: 10,
   },
-  roast: {
-    fontSize: 20,
-    color: "black",
+  separator: {
+    width: 200,
+    height: 2,
+    backgroundColor: "#30348c",
+    marginBottom: 10,
+  },
+  distance: {
+    fontSize: 18,
+    color: "#30348c",
+    marginBottom: 10,
   },
   map: {
-    width: "80%",
-    height: "50%",
+    width: "95%",
+    height: "80%",
     borderRadius: 20,
-    margin: 15,
+    marginTop: 10,
   },
   marker: {
     paddingVertical: 5,
@@ -152,8 +142,8 @@ const styles = StyleSheet.create({
   color: {
     color: "#30348c",
   },
-  home: {
+  button: {
     color: "#30348c",
-    position: "flex",
+    marginTop: 10,
   },
 });
