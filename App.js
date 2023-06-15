3fb966da92652dedc7cf0e0fb7439ba421724ef3
Moritz Henzi <moritz.henzi@bict-edu.ch>
2023-06-15 16:49:02 +0200

    feat: Salary Calculator works

--- App.js
+++ App.js
@@ -1,11 +1,135 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 export default function App() {
+  const [selectedProfession, setSelectedProfession] = useState('');
+  const [selectedYear, setSelectedYear] = useState('');
+  const [selectedMonth, setSelectedMonth] = useState('');
+
+  const professions = ['Informatiker/in EFZ', 'Mediamatiker/in EFZ'];
+  const years = ['1. Lehrjahr', '2. Lehrjahr', '3. Lehrjahr', '4. Lehrjahr'];
+  const months = [
+    { label: '1 Monat', value: 1 },
+    { label: '2 Monate', value: 2 },
+    { label: '3 Monate', value: 3 },
+    { label: '4 Monate', value: 4 },
+    { label: '5 Monate', value: 5 },
+    { label: '6 Monate', value: 6 },
+    { label: '7 Monate', value: 7 },
+    { label: '8 Monate', value: 8 },
+    { label: '9 Monate', value: 9 },
+    { label: '10 Monate', value: 10 },
+    { label: '11 Monate', value: 11 },
+    { label: '12 Monate', value: 12 },
+  ];
+
+
+
+
+
+  function rechner() {
+
+    var lohn = 0;
+    switch(selectedProfession) {
+      case 'informatiker':
+        switch(selectedYear) {
+          case '1. Lehrjahr':
+            lohn = 0
+            
+            break;
+          case '2. Lehrjahr':
+            lohn = 400
+            
+            break;
+          case '3. Lehrjahr':
+            lohn = 600
+            
+            break;
+          case '4. Lehrjahr':
+            lohn = 900
+           
+            break;
+        }
+        break;
+      case 'mediamatiker':
+        switch(selectedYear) {
+          case '1. Lehrjahr':
+            lohn = 0
+            
+            break;
+          case '2. Lehrjahr':
+            lohn = 0
+           
+            break;
+          case '3. Lehrjahr':
+            lohn = 600
+            
+            break;
+          case '4. Lehrjahr':
+            lohn = 900
+           
+            break;
+        }
+        break;
+
+        
+      }
+
+      //console.log(lohn * selectedMonth);
+      return `${lohn * selectedMonth} .-`
+}
+
+
+  const lohnrechner = rechner();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.title}>Salary Calculator</Text>
+        <View style={styles.separator} />
+      </View>
+      <Text style={styles.ergebniss}>{lohnrechner}</Text>
+      <View style={{marginBottom: 100}}>
+        <View style={styles.dropdownContainer}>
+          <Text style={styles.label}></Text>
+          <Picker
+            style={styles.dropdown}
+            selectedValue={selectedProfession}
+            onValueChange={(itemValue) => setSelectedProfession(itemValue)}
+          >
+            <Picker.Item label="Informatiker/in EFZ" value="informatiker" />
+            <Picker.Item label="Mediamatiker/in EFZ" value="mediamatiker" />
+          </Picker>
+        </View>
+
+        <View style={styles.dropdownContainer}>
+          <Text style={styles.label}></Text>
+          <Picker
+            style={styles.dropdown}
+            selectedValue={selectedYear}
+            onValueChange={(itemValue) => setSelectedYear(itemValue)}
+          >
+            {years.map((year, index) => (
+              <Picker.Item key={index} label={year} value={year} />
+            ))}
+          </Picker>
+        </View>
+
+        <View style={styles.dropdownContainer}>
+          <Text style={styles.label}></Text>
+          <Picker
+            style={styles.dropdown}
+            selectedValue={selectedMonth}
+            onValueChange={(itemValue) => setSelectedMonth(itemValue)}
+          >
+            {months.map((month) => (
+              <Picker.Item key={month.value} label={month.label} value={month.value} />
+
+            ))}
+          </Picker>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +137,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: 'space-between',
+    paddingTop: 60,
+    paddingBottom: 20,
   },
+  dropdownContainer: {
+    
+    justifyContent: "flex-end",
+    
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: -10,
+  },
+  dropdown: {
+    width: 200,
+    height: 40,
+    borderColor: '#333',
+    borderWidth: 1,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#30348c",
+    marginBottom: 10,
+    alignSelf: "center",
+    
+    
+  },
+  separator: {
+    width: 200,
+    height: 2,
+    backgroundColor: "#30348c",
+    marginBottom: 10,
+  },
+  ergebniss: {
+    fontSize: 48,
+    fontWeight: "bold",
+  }
 });
