da5a6ee30e6f3f68701da1d9a34361d1d7f63a3d
MonsterLuni <monsterluni@gmail.com>
2023-06-15 13:40:25 +0200

    feat MusicPlayer: Add Function to Start/Stop Sound

--- App.js
+++ App.js
@@ -6,20 +6,31 @@ import { Audio } from 'expo-av'
 export default function App() {
 
   const [sound, setSound] = useState();
+  var [Status, setStatus] = useState(false);
 
-  async function playSound() {
-    console.log('Loading Sound');
-    const {sound} = await Audio.Sound.createAsync(require("./assets/Music/a.mp3"));
-    setSound(sound);
-
-    console.log('Playing Sound');
-    await sound.playAsync();
+  async function changestateofSound() {
+    if(Status == true){
+      sound.stopAsync();
+      console.log("GESTOPPT");
+      setStatus(false);
+    }
+    else{
+      console.log('Loading Sound');
+      const {sound} = await Audio.Sound.createAsync(require("./assets/Music/Resonance.mp3"));
+      setSound(sound);
+      console.log('Playing Sound');
+      await sound.playAsync();
+      console.log(Status);
+    
+      setStatus(true);
+      console.log(Status);
+    }
   }
 
   return (
     <View style={styles.container}>
       <Text>Dies wird der MusicPlayer!</Text>
-      <Button title="Play Sound" onPress={playSound} />
+      <Button title="Play Sound" onPress={changestateofSound} />
       <StatusBar style="auto" />
     </View>
   );
