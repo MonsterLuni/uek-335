063d957fd024e646c0c35abcf8d2fdfa360c8016
MonsterLuni <monsterluni@gmail.com>
2023-06-15 11:38:24 +0200

    feat MusicPlayer: Add Ability to Play Sound

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Audio } from 'expo-av'
 
 export default function App() {
+
+  const [sound, setSound] = useState();
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const {sound} = await Audio.Sound.createAsync(require("./assets/Music/a.mp3"));
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+
   return (
     <View style={styles.container}>
       <Text>Dies wird der MusicPlayer!</Text>
+      <Button title="Play Sound" onPress={playSound} />
       <StatusBar style="auto" />
     </View>
   );
