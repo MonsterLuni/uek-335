92c4169c30418b030a9685aaf1dc3786bfeab5f1
MonsterLuni <monsterluni@gmail.com>
2023-06-21 11:52:04 +0200

    feat HomePage: Add Image, Title

--- Pages/Home.js
+++ Pages/Home.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Text } from "react-native";
-
+import { StyleSheet, Text } from "react-native";
+import { View } from "react-native";
+import { Image } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 export default function Home() {
   return (
-    <Text>das ist home</Text>
+    <View style={styles.container}>
+    <Image source={require('../assets/img/bict.png')} style={styles.img}/>
+    <Text style={styles.title}>Wilkommen in der BiCT App!</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 60,
+    paddingBottom: 20,
+  },
+  img: {
+    width: 350,
+    height: 145,
+    marginBottom: 60
+  },
+  title: {
+    fontSize: 30,
+  }
+})
